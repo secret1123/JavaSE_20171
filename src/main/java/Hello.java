735446068ec7ef87c00cb7cad9e2dcdd480829c1
secret1123 <198584an>
2017-03-06 16:18:47 +0800

    update

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -1,8 +1,21 @@
 /**
  * Created by anlu on 2017/3/6.
  */
+
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello,Java!");
+
+        int x = 107;
+        int y = 299;
+        int z = x + y;
+        System.out.println(z);
+
+        if (x + y > 400) {
+            System.out.println("今天星期一");
+        }
     }
 }
+
+
+
