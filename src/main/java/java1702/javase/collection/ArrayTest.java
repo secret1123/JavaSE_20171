0fc531cf0e4b172bac042342be1dfa92024c9e02
secret1123 <198584an>
2017-03-27 16:41:52 +0800

    update

---
+++ src/main/java/java1702/javase/collection/ArrayTest.java
@@ -0,0 +1,51 @@
+package java1702.javase.collection;
+
+/**
+ * Created by AnLu on
+ * 2017/3/27 15:34.
+ * JavaSE_20171
+ */
+public class ArrayTest {
+    public static void main(String[] args) {
+        String[] strings = new String[10];
+        System.out.println(strings[0]);
+        strings[0] = "test.";
+        System.out.println(strings[0]);
+
+        boolean[] booleans = new boolean[100];
+        System.out.println(booleans[99]);
+
+        int[] ints = {1,2,3,4,5,6,67,888};
+        System.out.println(ints[6]);
+
+        double[] doubles= new double[10000];
+        for (int i = 0; i < doubles.length; i++) {
+            doubles[i] = i + 1;
+        }
+        System.out.println("----->"+doubles[doubles.length-1]);
+        for (int i = 0; i < doubles.length; i++) {
+            System.out.println(doubles[i]);
+        }
+
+        ArrayTest[] arrayTests = {new ArrayTest(),new ArrayTest(),new ArrayTest()};
+        System.out.println(arrayTests[0]);
+        System.out.println(arrayTests[1]);
+        System.out.println(arrayTests[2]);
+        System.out.println(arrayTests.length);
+
+        String[] months = {"Jan","Feb","Mar","Apr","May"};
+
+        //itar + tab
+        for (int i = 0; i < months.length; i++) {
+            String month = months[i];
+            System.out.println(month);
+        }
+
+        System.out.println("---------------");
+
+        //iter + tab
+        for (String month : months) {
+            System.out.println(month);
+        }
+    }
+}
