505b80ee8423377f58355d04f59ad2d4eed00adf
secret1123 <19072390@qq.com>
2017-03-29 11:08:10 +0800

    update

--- src/main/java/java1702/javase/collection/StringVsStringBufferTest.java
+++ src/main/java/java1702/javase/collection/StringVsStringBufferTest.java
@@ -6,4 +6,19 @@ package java1702.javase.collection;
  * JavaSE_20171
  */
 public class StringVsStringBufferTest {
+
+    private static final String ALPHABET = "abcdefghijklmnopqrstuvwxyz";
+    public static void main(String[] args) {
+
+        String s = "";
+        StringBuffer stringBuffer = new StringBuffer("");
+        long start = System.currentTimeMillis();
+        for (int i = 0; i < 1000; i++) {
+//            s += ALPHABET;
+            stringBuffer.append(ALPHABET);
+        }
+        System.out.println("time:"+(System.currentTimeMillis()-start));
+//        System.out.println(s.length());//42s
+        System.out.println(stringBuffer.length());//0s
+    }
 }
