4204d53bf5e2b9208d14ad860a36885f9995ade4
secret1123 <19072390@qq.com>
2017-03-31 21:02:39 +0800

    update

--- src/main/java/java1702/javase/collection/ListTest.java
+++ src/main/java/java1702/javase/collection/ListTest.java
@@ -2,6 +2,7 @@ package java1702.javase.collection;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 /**
  * Created by AnLu on
@@ -20,32 +21,32 @@ public class ListTest extends ArrayList{
         arrayList.add("args");
         arrayList.add("args");
         arrayList.add("args");
-        arrayList.add("args");
-        arrayList.add("args");
-        arrayList.add("args");
-        arrayList.add("args");
-        arrayList.add("args");
-        arrayList.add("args");
-        arrayList.add("args");
-        arrayList.add("args");
-        arrayList.add("args");
-        arrayList.add("args");
-        arrayList.add("args");
-        arrayList.add("args");
-        arrayList.add("args");
-        arrayList.add("args");
         System.out.println(arrayList.size());
         System.out.println(arrayList.get(2));
         System.out.println(arrayList.contains("main"));
         System.out.println(arrayList.indexOf("applet"));
         System.out.println(arrayList.lastIndexOf("args"));
         System.out.println(arrayList.isEmpty());
-//        System.out.println(arrayList.set(2,"me"));
-//        for (String string : arrayList) {
-//            System.out.println(string);
-//        }
-        System.out.println(Arrays.toString(arrayList.toArray()));
-        arrayList.ensureCapacity(15);
+        System.out.println(arrayList.set(2,"me"));
+        for (String string : arrayList) {
+            System.out.println(string);
+        }
+        System.out.println(arrayList.remove(4));
+        Object[] objects = arrayList.toArray();
+        arrayList.add("main");
+        System.out.println(arrayList.size());
+        System.out.println(arrayList);
+        ArrayList<String> strings = new ArrayList<>();
+        arrayList.removeAll(strings);
+        System.out.println(arrayList);
+        System.out.println(arrayList.size());
+        arrayList.trimToSize();
+        System.out.println(arrayList.size());
+//        System.out.println(Arrays.toString(arrayList.toArray()));
+//        arrayList.ensureCapacity(15);
+
+        List<String> strings1 = arrayList.subList(2,4);
+        System.out.println(strings1);
 
         ListTest listTest = new ListTest();
         for (int i = 0; i < 10; i++) {
