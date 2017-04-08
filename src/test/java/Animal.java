7b9f3aaa8f429e320bc1ac6c9867d602dab3e7f0
secret1123 <19072390@qq.com>
2017-04-08 16:58:57 +0800

    update

--- src/test/java/Animal.java
+++ src/test/java/Animal.java
@@ -1,23 +1,17 @@
 /**
  * Created by AnLu on
- * 2017/3/25 09:22.
+ * 2017/4/8 14:14.
  * JavaSE_20171
  */
-//创建一个 Animal 动物类，定义三个域和三个方法
-public class Animal {
-    int limbs;//四肢
-    String coat;//皮毛
-    String tail;//尾巴
+//    按以下要求编写程序
+//            (1) 编写 Animal 接口，接口中声明 eat() 方法
+//            (2) 定义Bird类和 Fish 类实现 Animal 接口,Bird 中还有 flying 方法，Fish 中还有 swimming 方法，分别实现这些方法
+//            (3) 编写 Bird 类和 Fish 类的测试程序，并调用其中的各个方法
+interface Animal {
 
-    public void reptile() { //爬
-
-    }
-
-    public void cry() { //叫
-
-    }
-
-    public void bite() { //咬
-
-    }
+    void eat();
 }
+
+
+
+
