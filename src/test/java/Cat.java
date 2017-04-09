46b321f8d4c908746020d47d626d137281311e06
secret1123 <19072390@qq.com>
2017-04-09 19:22:17 +0800

    update

--- src/test/java/Cat.java
+++ src/test/java/Cat.java
@@ -5,7 +5,7 @@
  */
 //定义一个 Animal 类的子类 Cat，新增两个域和两个方法
 //在 Cat 类中重写父类 Animal 的三个方法
-public class Cat extends Animal {
+public class Cat extends Animal1 {
 
     String paws;//爪子
     String ear;//耳朵
---
+++ src/test/java/Scientist.java
@@ -0,0 +1,12 @@
+/**
+ * Created by AnLu on
+ * 2017/4/8 16:27.
+ * JavaSE_20171
+ */
+public class Scientist extends Jobs {
+    String scientist;//科学家
+
+    public double bonus() {//奖金
+        return 0.0;
+    }
+}
