8b63d10f812dbc5fda48d559acfb151f044840da
secret1123 <19072390@qq.com>
2017-04-19 09:14:27 +0800

    update

---
+++ src/main/java/io/UrlTest.java
@@ -0,0 +1,24 @@
+package io;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+/**
+ * Created by AnLu on
+ * 2017/4/18 18:52.
+ * JavaSE_20171
+ */
+public class UrlTest {
+    public static void main(String[] args) throws IOException {
+
+        try {
+            URL url = new URL("http://localhost:63342/JavaSE_20171/shuixiang.jpg");
+            InputStream stream = url.openStream();
+            System.out.println(stream.read());
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/main/java/java1702/javase/exercise/StringTest1.java
@@ -0,0 +1,14 @@
+package java1702.javase.exercise;
+
+/**
+ * Created by AnLu on
+ * 2017/4/16 19:11.
+ * JavaSE_20171
+ */
+//输入字符串长度len1，字符串s1，字符串长度len
+// 2，字符串s2。从后向前比较，以最短字符串为标准，输出不同的元素的个数。
+//例如:
+//        输入：s1=”1,3,5” len1=3 s2=”2,4,1,7,5” len2=5
+//        输出：1
+public class StringTest1 {
+}
