1c018d32dfd135c5c563d52f9e99749cecc09fbf
secret1123 <19072390@qq.com>
2017-04-26 09:44:28 +0800

    update

--- src/main/java/java1702/javase/io/ExtensionNameTest.java
+++ src/main/java/java1702/javase/io/ExtensionNameTest.java
@@ -6,20 +6,10 @@ package java1702.javase.io;
  * JavaSE_20171
  */
 public class ExtensionNameTest {
-//    public static void main(String[] args) {
-//        String s = "123.gif";
-//        System.out.println(s.substring(s.lastIndexOf('.')));
-
-        public static long fibonacii(int n){
-            if (n == 1 || n == 2) {
-                return 1;
-            }
-            return fibonacii(n-2)+fibonacii(n-1);
-        }
-        public static void main(String[] args) {
-            for (int i = 0; i < 5; i++) {
-                System.out.println(fibonacii(i));
-            }
-            System.out.println(fibonacii(6));
-        }
+    public static void main(String[] args) {
+        String s = "123.gif";
+        System.out.println(s.substring(s.lastIndexOf('.')));
     }
+}
+
+
