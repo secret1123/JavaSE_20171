696791d847a26fa6658dfc8b6798028fece8fe55
secret1123 <19072390@qq.com>
2017-04-28 13:36:51 +0800

    update

--- src/test/java/Test5.java
+++ src/test/java/Test5.java
@@ -6,26 +6,26 @@ import java.io.RandomAccessFile;
  * 2017/4/22 10:02.
  * JavaSE_20171
  */
-public class Test5{
+public class Test5 {
 
-    private static int fibonacci(int n){
-        if (n==1||n==2) {
+    private static int fibonacci(int n) {
+        if (n == 1 || n == 2) {
             return 1;
         }
-        return fibonacci(n-1)+fibonacci(n-2);
+        return fibonacci(n - 1) + fibonacci(n - 2);
     }
 
     public static void main(String[] args) {
         try
-                (RandomAccessFile raf = new RandomAccessFile("raf.txt","rw")){
+                (RandomAccessFile raf = new RandomAccessFile("raf.txt", "rw")) {
             for (int i = 0; i < 20; i++) {
-                raf.writeInt(fibonacci(i+1));
+                raf.writeInt(fibonacci(i + 1));
             }
             raf.seek(0);
             for (int i = 0; i < 3; i++) {
                 raf.readInt();
-                try (RandomAccessFile file = new RandomAccessFile("output.txt","rw")){
-                    int r =raf.readInt();
+                try (RandomAccessFile file = new RandomAccessFile("output.txt", "rw")) {
+                    int r = raf.readInt();
                     System.out.println(r);
                     file.writeInt(r);
                 }
