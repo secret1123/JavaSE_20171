4c63f9efd6e12ac6cccec802ae77adabc4312130
secret1123 <19072390@qq.com>
2017-05-03 20:01:42 +0800

    update

---
+++ src/main/java/java1702/javase/reflect/Human.java
@@ -0,0 +1,116 @@
+package java1702.javase.reflect;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+
+/**
+ * Created by AnLu on
+ * 2017/5/3 17:32.
+ * JavaSE_20171
+ */
+class Animals{
+    public int age;
+    private double weight;
+
+    public Animals() {
+    }
+
+    public Animals(int age, double weight) {
+        this.age = age;
+        this.weight = weight;
+    }
+
+    public int sleep(int hours){
+        return hours;
+    }
+
+    public void eat(String food){
+        System.out.println("eating"+food);
+    }
+
+    private void killHuman(){
+        System.out.println("killed a poor guy.");
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+}
+
+public class Human extends Animals{
+    public String name;
+    private boolean married;
+
+    public Human() {
+    }
+
+    public Human(String name, boolean married) {
+        this.name = name;
+        this.married = married;
+    }
+
+    public Human(int age, double weight, String name, boolean married) {
+        super(age, weight);
+        this.name = name;
+        this.married = married;
+    }
+
+    @Override
+    public void eat(String food) {
+        System.out.println(name+"is now eating"+food);
+    }
+
+    public void study(String course){
+        System.out.println(name+"is now studying"+course);
+    }
+
+    public void killAnimals(String animal){
+        System.out.println(name+"is now killing"+animal);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isMarried() {
+        return married;
+    }
+
+    public void setMarried(boolean married) {
+        this.married = married;
+    }
+}
+
+class HumanTest{
+    public static void main(String[] args) {
+        Human human = new Human();
+        Class clazz = human.getClass();
+        Field[] fields = clazz.getFields();
+        System.out.println("------getFields()-------");
+        for (Field field : fields) {
+            System.out.println(field.getName());
+        }
+
+        Field[] declaredFilds = clazz.getDeclaredFields();
+        System.out.println("------getDeclaredFields()------");
+        for (Field declaredFild : declaredFilds) {
+            System.out.println(declaredFild.getName());
+        }
+    }
+}
