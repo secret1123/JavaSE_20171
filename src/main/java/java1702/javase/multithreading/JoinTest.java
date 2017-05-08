e3dd7f4acc7eba0ceb9f31187c11445bc258dcc2
secret1123 <19072390@qq.com>
2017-05-08 16:53:49 +0800

    update

--- src/main/java/java1702/javase/multithreading/JoinTest.java
+++ src/main/java/java1702/javase/multithreading/JoinTest.java
@@ -5,6 +5,25 @@ package java1702.javase.multithreading;
  * 2017/5/8 16:45.
  * JavaSE_20171
  */
-public class JoinTest {
+public class JoinTest implements Runnable{
 
+    public static void main(String[] args) {
+        Thread thread =new Thread(new JoinTest());
+        thread.start();
+
+        try {
+            thread.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("test...");
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
+    }
 }
