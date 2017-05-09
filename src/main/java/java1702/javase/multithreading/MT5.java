90d3b024714d2d1cd3f953ce13127b4c2c381f0e
secret1123 <19072390@qq.com>
2017-05-09 10:30:49 +0800

    update

--- src/main/java/java1702/javase/multithreading/MT5.java
+++ src/main/java/java1702/javase/multithreading/MT5.java
@@ -9,12 +9,12 @@ public class MT5 implements Runnable{
 
     public static void main(String[] args) {
 
-        Thread thread1 = new Thread(new MT5());
+        Thread thread1 = new Thread(new MT5(),"thread 1");
         thread1.setName("thread1");
-        Thread thread2 = new Thread(new MT5());
-        thread2.setName("thread2");
-        Thread thread3 = new Thread(new MT5());
-        thread3.setName("thread3");
+        Thread thread2 = new Thread(new MT5(),"thread 2");
+//        thread2.setName("thread2");
+        Thread thread3 = new Thread(new MT5(),"thread 3");
+//        thread3.setName("thread3");
 
         thread1.start();
         thread2.start();
@@ -26,6 +26,12 @@ public class MT5 implements Runnable{
 
         thread3.start();
 
+        try {
+            thread3.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
         System.out.println("test.");
     }
 
@@ -34,11 +40,10 @@ public class MT5 implements Runnable{
         for (int i = 0; i < 3; i++) {
             System.out.println(Thread.currentThread().getName()+"is running...");
             try {
-                Thread.sleep((int)(Math.random()*1000));
+                Thread.sleep((int)(Math.random()*5000));
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
         }
-
     }
 }
---
+++ src/main/java/java1702/javase/multithreading/YieldTest.java
@@ -0,0 +1,28 @@
+package java1702.javase.multithreading;
+
+/**
+ * Created by AnLu on
+ * 2017/5/9 09:13.
+ * JavaSE_20171
+ */
+public class YieldTest implements Runnable {
+
+    public static void main(String[] args) {
+        Thread thread1 = new Thread(new YieldTest(),"thread 1");
+        Thread thread2 = new Thread(new YieldTest(),"thread 2");
+
+        thread1.start();
+        thread2.start();
+
+        System.out.println("test...");
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 100; i++) {
+            if (i%10==0){
+                Thread.yield();
+            }
+        }
+    }
+}
